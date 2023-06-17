7aa8ee529ffad9b97b236a2f69993cf921db5b36
Gabriel Marquette <gab64320@gmail.com>
2023-06-17 17:29:52 +0200

    Ajout de la Cross Guild & Teach's Crew

--- GuessWho/app/src/main/java/fr/gmarquette/guesswho/GameData/Characters/Pirates.java
+++ GuessWho/app/src/main/java/fr/gmarquette/guesswho/GameData/Characters/Pirates.java
@@ -90,12 +90,51 @@ class Pirates {
 
     private static void TeachCrew()
     {
+        Characters teach = new Characters("Marshall D. Teach", true, "3996000000", 223, "Pirate", true, 40, "Teach's Crew", 0);
+        Characters burgess = new Characters("Jesus Burgess", true, "20000000", 222, "Pirate", true, 29, "Teach's Crew", 0);
+        Characters shiliew = new Characters("Shiliew", true, "Unknown", 538, "Pirate", true, 44, "Teach's Crew", 0);
+        Characters auger = new Characters("Van Auger", true, "64000000", 222, "Pirate", true, 27, "Teach's Crew", 0);
+        Characters pizarro = new Characters("Avalo Pizarro", true, "Unknown", 575, "Pirate", true, 42, "Teach's Crew", 0);
+        Characters laffitte = new Characters("Laffitte", true, "42200000", 234, "Pirate", true, 41, "Teach's Crew", 0);
+        Characters devon = new Characters("Catarina Devon", true, "Unknown", 575, "Pirate", true, 36, "Teach's Crew", 0);
+        Characters wolf = new Characters("Sanjuan Wolf", true, "Unknown", 575, "Pirate", true, 99, "Teach's Crew", 0);
+        Characters vasco = new Characters("Vasco Shot", true, "Unknown", 575, "Pirate", true, 38, "Teach's Crew", 0);
+        Characters doq = new Characters("Doc Q", true, "72000000", 223, "Pirate", true, 28, "Teach's Crew", 0);
+        Characters kuzan = new Characters("Kuzan / Aokiji", true, "Unknown", 303, "Pirate", true, 49, "Teach's Crew", 0);
+        //Characters pinkBeard = new Characters("Barbe Rose", true, "52000000", 904, "Pirate", true, ??, "Teach's Crew", 1);
 
+        listCharacters.add(teach);
+        listCharacters.add(burgess);
+        listCharacters.add(shiliew);
+        listCharacters.add(auger);
+        listCharacters.add(pizarro);
+        listCharacters.add(laffitte);
+        listCharacters.add(devon);
+        listCharacters.add(wolf);
+        listCharacters.add(vasco);
+        listCharacters.add(doq);
+        listCharacters.add(kuzan);
     }
 
     private static void CrossGuild()
     {
+        Characters baggy = new Characters("Baggy", true, "3189000000", 9, "Pirate", true, 39, "Cross Guild", 0);
+        Characters mihawk = new Characters("Dracule Mihawk", false, " 3590000000", 49, "Pirate", true, 43, "Cross Guild", 0);
+        Characters crocodile = new Characters("Crocodile", true, "1965000000", 126, "Pirate", true, 46, "Cross Guild", 0);
+        Characters das = new Characters("Das Bones / Mr 1", true, "75000000", 160, "Pirate", true, 31, "Cross Guild", 0);
+        Characters alvida = new Characters("Alvida", true, "5000000", 2, "Pirate", true, 27, "Cross Guild", 0);
+        Characters galdino = new Characters("Galdino / Mr 3", true, "24000000", 117, "Pirate", true, 37, "Cross Guild", 1);
+        Characters morge = new Characters("Morge", false, "Unknown", 9, "Pirate", true, 29, "Cross Guild", 1);
+        Characters cabadji = new Characters("Cabadji", false, "Unknown", 9, "Pirate", true, 34, "Cross Guild", 1);
 
+        listCharacters.add(baggy);
+        listCharacters.add(mihawk);
+        listCharacters.add(crocodile);
+        listCharacters.add(das);
+        listCharacters.add(alvida);
+        listCharacters.add(galdino);
+        listCharacters.add(morge);
+        listCharacters.add(cabadji);
     }
 
     private static void NewgateCrew()
